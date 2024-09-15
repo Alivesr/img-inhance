225dd1d3339460fc47bd297b45073c125177d0a7
刘展豪 <3101613795@qq.com>
2024-09-15 21:54:17 +0800

    基本前端预览

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-upload
+    v-model:file-list="fileList"
+    class="upload-demo"
+    :before-upload="handleUpload"
+    :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    list-type="picture"
+    multiple
+  >
+    <el-button type="primary">Click to upload</el-button>
+    <template #tip >    <div class="el-upload__tip">
+        jpg/png files with a size less than 500kb
+      </div>
+    </template>
+  </el-upload>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import type { UploadProps, UploadUserFile } from 'element-plus'
+
+const fileList = ref<UploadUserFile[]>([
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+  },
+  {
+    name: 'food2.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+  },
+])
+
+const handleUpload = (rawFile: File) => {
+  const reader = new FileReader()
+  reader.onload = (event) => {
+    const url = event.target?.result as string
+    fileList.value.push({
+      name: rawFile.name,
+      url,
+    })
+  }
+  reader.readAsDataURL(rawFile)
+  return false // Prevent default upload behavior
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+  fileList.value = uploadFiles
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+const handlePreview: UploadProps['onPreview'] = (file) => {
+  console.log(file)
 }
-</style>
+</script>
